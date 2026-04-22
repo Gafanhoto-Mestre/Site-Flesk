10d2be7dc1421ce55849e67d7f206e617713f91e
Gafanhoto-Mestre <134173192+Gafanhoto-Mestre@users.noreply.github.com>
2026-04-21 21:55:46 -0300

    Criando site com flesk
    Copiei tudo para a pasta

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Dados simulados (No futuro virão do Banco de Dados)
+cardapio_db = [
+    {"id": 1, "nome": "Macarrão Tradicional", "descricao": "Massa artesanal e legumes frescos.", "preco": 25.00},
+    {"id": 2, "nome": "Especial de Bacon", "descricao": "Bacon crocante e muito queijo.", "preco": 32.00},
+    {"id": 3, "nome": "Frango com Catupiry", "descricao": "Frango desfiado com Catupiry original.", "preco": 28.50}
+]
+
+@app.route('/')
+def index():
+    return render_template('cardapio.html', pratos=cardapio_db)
+
+@app.route('/carrinho')
+def carrinho():
+    return render_template('carrinho.html')
+
+if __name__ == '__main__':
+    # host='0.0.0.0' permite que você acesse pelo IP do PC no celular
+    app.run(debug=True, host='0.0.0.0')
